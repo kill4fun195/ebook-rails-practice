0f816f829ec55f20c74cd754c2480932e43c4859
kill4fun195 <vandao12031995@gmail.com>
2016-04-23 18:44:33 +0700

    update website

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,31 +1,6 @@
 $(document).ready(function(){
-  $(".notice").hide();
-  $(".no-login-article p a").click(function(){
-    $("#user_email").val("");
-    $("#user_password").val("");
-  })
-  $("body").on("click",".modal-content form #loggin-account ",function(event){
-    form = $(this).parents("form");
-      $.ajax({
-          url: form.attr("action"),
-          type: "POST",
-      data: {"user": {
-          email: $("#user_email").val(),
-          password: $("#user_password").val()
-        }
-      },
-      success: function(response) {
-        if($(response).find(".notice").text()=="invalid")
-        {
-          alert("dang nhap sai");
-        }
-        if($(response).find(".notice").text()=="logged")
-        {
-          alert("dang nhap thanh cong");
-          window.location = window.location
-        }
-      }
-      });
-      return false;
+  $("#create-account").click(function(){
+    _this = $(this);
+    _this.attr("disabled",true);
   });
 });
