5b76d4fc04f0cbb7bab52027b33548c72378eb50
kill4fun195 <vandao12031995@gmail.com>
2016-05-19 00:14:29 +0700

    Update postgres gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
+  #association
   has_many :articles , dependent: :destroy
   has_many :comments , dependent: :destroy
-  
+
+  #validation
+  validates :email, confirmation: true
+  validates :name_user, length: { minimum: 5 }
+  validates :password, length: { in: 3..20 }
+  validates :role, presence: true
+
+  #search
   def authenticate(user_password)
     self.password == user_password
   end
