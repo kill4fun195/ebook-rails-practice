1516b4faea23c2055524cc234d8b20fcc7ddc511
kill4fun195 <vandao12031995@gmail.com>
2016-06-03 23:49:28 +0700

    update add image url in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name_user = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
+      user.image = auth.info.image # assuming the user model has an image
     end
   end
 
