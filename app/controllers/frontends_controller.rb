eccfc05fda8923847d23adfa2733df865aca3f0e
kill4fun195 <vandao12031995@gmail.com>
2016-07-23 00:45:45 +0700

    Update Css in Web

--- app/controllers/frontends_controller.rb
+++ app/controllers/frontends_controller.rb
@@ -5,15 +5,15 @@ class FrontendsController < ApplicationController
 
   def index
     if params[:search]
-      @articles = Article.includes(:categories,:comments).order_desc.search(params[:search]).page(params[:page]).per_page(5)
+      @articles = Article.includes(:categories,:comments).order_desc.search(params[:search]).page(params[:page]).per_page(6)
     else
-      @articles = Article.includes(:categories,:comments).order_desc.page(params[:page]).per_page(5)
+      @articles = Article.includes(:categories,:comments).order_desc.page(params[:page]).per_page(6)
     end
     @a = params[:page].to_i 
     if (@articles.count % 2 == 0 )
-      @t = (@articles.count / 5) 
+      @t = (@articles.count / 6) 
     else
-      @t = (@articles.count / 5) + 1
+      @t = (@articles.count / 6) + 1
     end
 
   end
